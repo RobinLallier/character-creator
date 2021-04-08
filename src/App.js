86c55e9a840485af77babe6f9aec6dcf282fdcc8
Robin Lallier <33777989+RobinLallier@users.noreply.github.com>
2021-04-08 20:42:08 +0200

    Completing desktop objectives

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          width: "100%",
+          height: "40%",
+          backgroundColor: " hsl(195deg, 20%, 86%)",
+          position: "fixed",
+          bottom: 0,
+        }}
+      />
       <CharacterEditor />
       <Footer />
     </>
